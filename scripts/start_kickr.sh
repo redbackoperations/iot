bf1017d47626e4e7b7067cd39dc76b156ca33745
Adrian Grigo <agrigo2001@yahoo.com.au>
2022-09-21 05:39:17 +0100

    NEW: Scripts to start each service on the Raspberry Pi

---
+++ scripts/start_kickr.sh
@@ -0,0 +1,2 @@
+source ~/.env
+~/iot/Drivers/kickr_climb_and_smart_trainer/incline_and_resistance_control.py --mac_address ${KICKR_MAC_ADDRESS} --broker_address ${MQTT_HOSTNAME} --username ${MQTT_USERNAME} --password ${MQTT_PASSWORD} --resistance_command_topic /bike/${DEVICE_ID}/resistance --resistance_report_topic /bike/${DEVICE_ID}/resistance/report --incline_command_topic /bike/${DEVICE_ID}/incline --incline_report_topic /bike/${DEVICE_ID}/incline/report
