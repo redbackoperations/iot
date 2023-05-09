013d951fbdb744c009217e1675785681c9a2beb2
e-benj <101461576+e-benj@users.noreply.github.com>
2023-05-09 22:47:36 +0200

    Create wlan0_update_bot.py

---
+++ IPUpdater/wlan0_update_bot.py
@@ -0,0 +1,16 @@
+import os
+
+import telebot
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+with open("eth0.txt") as file:
+        ip = file.readline()
+
+file.close()
+
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
+
+message = "Latest wlan0 IP config for Redback Pi 0000001: " + ip
+
+bot.send_message(-1001950776693, message)
