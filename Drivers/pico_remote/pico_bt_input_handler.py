1c5edecbe205ba3123ab519ed024ab948d35519f
Thumbness <face.first@hotmail.com>
2023-08-02 21:30:30 +1000

    add MQTT client to package, write notes for implementation:

--- Drivers/pico_remote/pico_bt_input_handler.py
+++ Drivers/pico_remote/pico_bt_input_handler.py
@@ -1,8 +1,27 @@
 import bluetooth
+import sys
+import time
+import os
+from mqtt_client import MQTTClient
+from dotenv import load_dotenv
 
-hc06_address = "98:D3:31:40:60:B0"
+# TO DO: store MAC address in .env file and read from os.getenv
+
+hc06_address = os.getenv('HC06_ADDRESS')
 port = 1
+env_path = '/' #/home/pi/.env for lab pi
+load_dotenv(env_path)
+mqtt_client = MQTTClient(os.getenv('MQTT_HOSTNAME'), os.getenv('MQTT_USERNAME'), os.getenv('MQTT_PASSWORD'))
 
+# TO DO:
+## On Start:
+# 1. Read from .env file
+# 2. Instantiate MQTT client and subscribe to reistance and incline topic
+# 3. Read from topic to get current resistance and incline (published messages must be set to retain = True, check Drivers)
+## On Loop:
+# 4. Listen for pico BT input (increase/decrease resistance)
+# 5. Update local global resistance and incline variables
+# 6. Publish resistance and incline to topic
 
 print("Connecting to socket..")
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
--- Drivers/pico_remote/pico_runtime.py
+++ Drivers/pico_remote/pico_runtime.py
@@ -1,15 +1,33 @@
+################## WARNING ##################
+# naming this file to main.py and storing it on the pico without any safety backup to return to the REPL will result in
+# bricking the pico pi, as the pico will boot straight into main.py at startup and become unresponsive to connectio attempts from
+# the Thonny IDE from that point forward.
+
+# Absolute mandatory to perform thorough testing of implementation with naming file to anything other than 'main.py'.
+# Only once going to production naming this file to main.py and flashing to pico will be necessary to boot this program on start-up
+
+###############################################
+
+
 from machine import Pin, UART
 import utime
 
-# Assume button is connected to GPIO pin 15
-button = Pin(2, Pin.IN, Pin.PULL_UP)
+
+increase_button = Pin(2, Pin.IN, Pin.PULL_UP)
+decrease_button = Pin(15, Pin.IN, Pin.PULL_UP)
 
 # Configure UART communication
+# We use Port TX/RX 1 between pico and pi at baudrate 9600
+
 uart = UART(1, 9600)
-cntr = 0
+
 while True:
-    if button.value() == 0:  # When button is pressed
-        cntr += 1
-        uart.write('Button Pressed\n')
-        print('Button Pressed: ', cntr)
-        utime.sleep_ms(300)  # Debounce delay
+    if increase_button.value() == 0:  # When button is pressed
+        uart.write('increase\n')
+        print('Increase Button Pressed: ')
+        utime.sleep_ms(1000)  # Debounce delay
+        
+    if decrease_button.value() == 0:  # When button is pressed
+        uart.write('decrease\n')
+        print('Decrease Button Pressed: ')
+        utime.sleep_ms(1000)  # Debounce delay
