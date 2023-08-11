5b136f4ea460423a7e646a73497e6091b20293fe
William Djojodiredjo <williamdjojodiredjo@gmail.com>
2023-08-11 15:43:36 +1000

    Update Threshold workout
    Update Threshold workout and make some skeleton method for next week's improvement

--- Drivers/Threshold_workout/Threshold_class.py
+++ Drivers/Threshold_workout/Threshold_class.py
@@ -1,78 +1,83 @@
 import os
 import time
+import json
 from dotenv import load_dotenv
 
 class ThresholdWorkout:
     def __init__(self):
-        load_dotenv('/home/pi/.env')
-        self.duration = 0
-        self.power_data = [0]
-        self.threshold_workout = os.environ.get("THRESHOLD_WORKOUT_SCORE")  # Updated variable name
+        load_dotenv('home/pi/.env')
+        self.threshold_workout = os.environ.get('THRESHOLD_WORKOUT_SCORE')
         self.current_power = 0
+        self.power_data = []
+        self.calories = 0
+        self.current_speed = 0
+        self.speed_data = []
+        self.distance = 0
+        self.interval = 0
+        self.duration = 0
+        self.rest = 0
         self.threshold_power = 0
-    
-    def set_threshold_workout(self, threshold_workout):  # Updated method name
-        self.threshold_workout = threshold_workout
-    
-    def get_threshold_workout(self):  # Updated method name
-        return self.threshold_workout
-    
+        
+    # set and get variables
+    def set_interval(self, interval):
+        self.interval = interval
+    def get_interval(self):
+        return self.interval    
+        
+    def set_duration(self, duration):
+        self.duration = duration
     def get_duration(self):
         return self.duration
     
-    def set_duration(self, duration):
-        self.duration = duration
-        
-    def get_power_data(self):
-        return self.power_data
-    
-    def set_power_data(self, input_data):
-        self.power_data = []
-        for x in input_data:
-            self.power_data.append(x)
-    
-    def calculate_threshold_workout(self):  # Updated method name
-        avg_power = sum(self.power_data) / len(self.power_data)
-        self.set_threshold_workout(avg_power * 0.95)
-        
-    def read_remote_data(self, client, userdata, msg):
-        payload = msg.payload.decode("utf-8")
-        try:
-            dict_of_payload = json.loads(payload)
-            power_value = dict_of_payload["value"]
-            temp = self.power_data[-1]
-            if temp != power_value:
-                print("Received " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))      
-            self.current_power = power_value
-            self.check_threshold()
-        except json.JSONDecodeError:
-            power_value = payload
+    def set_rest(self, rest):
+        self.rest = rest
+    def get_rest(self):
+        return self.rest
     
     def set_threshold_power(self, threshold):
         self.threshold_power = threshold
+    def get_threshold_power(self):
+        return self.threshold_power
     
+    # Check threshold
     def check_threshold(self):
         if self.current_power > self.threshold_power:
-            print("Threshold power exceeded!")
-            resistance_required = self.calculate_resistance_required(self.current_power)
-            print("Resistance required to reach threshold power: ", resistance_required)
-            # Perform threshold workout action
-            # Replace this with your desired action when the threshold is exceeded
+            print('Threshold power exceeded!')
     
-    def calculate_power_usage_per_second(self, start_time, power_usage):
-        elapsed_time = time.time() - start_time
-        if elapsed_time > 0:
-            return power_usage / elapsed_time
-        else:
-            return 0
+    # Future improvement - calculate distance travelled
+    def calculate_distance(self):
+        # formula - distance(m) = speed(m/s)*time(second)
+        pass
+    def get_distance(self):
+        return self.distance
     
-    def calculate_resistance_required(self, current_power):
-        # Calculate the resistance required to reach the threshold power
-        # I've used a hardcoded method
-        if current_power < 100:
-            resistance_required = 5
-        elif current_power < 200:
-            resistance_required = 8
-        else:
-            resistance_required = 10
-        return resistance_required
+    # Future improvement - calculate calories burned
+    def calculate_calories(self):
+        # formula - Cal(kcal) = avg_power(Watts)*time(hour)*3.6
+        pass
+    def get_calories(self):
+        return self.calories
+    
+    # Future improvement - calculate current power per second
+    def calculate_power_poer_second(self):
+        pass
+    
+    
+    # Receive message
+    def read_message(self, client, userdata, msg):
+        deviceId = os.getenv('DEVICE_ID')
+        if msg.topic == f'bike/{deviceId}/power':
+            power_payload = msg.payload.decode('utf-8')
+            dict_of_power_payload = json.loads(power_payload)
+            power_value = dict_of_power_payload['value']
+            # print("Received " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+            self.current_power = power_value
+            self.check_threshold()
+        # elif msg.topic == f'bike/{deviceId}/speed':
+        #     speed_payload = msg.payload.decode('utf-8')
+        #     dict_of_speed_payload = json.loads(speed_payload)
+        #     speed_value = dict_of_speed_payload["value"]
+        #     print("Received " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+        #     self.current_speed = speed_value
+        #     self.speed_data.append(speed_value)
+            
--- Drivers/Threshold_workout/Threshold_workout.py
+++ Drivers/Threshold_workout/Threshold_workout.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import sys
 import time
 import os
@@ -6,79 +5,120 @@ from mqtt_client import MQTTClient
 from Threshold_class import ThresholdWorkout
 from dotenv import load_dotenv, set_key
 
-def perform_threshold_test(threshold_workout_object):
-    print("Current Threshold Workout: ", threshold_workout_object.get_threshold_workout())
-    print("Starting threshold test in 5 seconds...")
-    time.sleep(5)
+def perform_threshold_workout(threshold_object):
+    # Countdown
+    for i in range(5):
+        print('Starting threshold workout in', 5-i)
+        time.sleep(1)
+    print('Good Luck!!!')
     
+    # Start timer
     start_time = time.time()
-    power_usage = 0
+    
+    # Fetch data from sensor while in workout duration
     try:
-        while time.time() - start_time < (threshold_workout_object.get_duration() * 60):
+        while time.time() - start_time < (threshold_object.get_duration()*60):
             time.sleep(1)
-            if threshold_workout_object.get_power_data() is not None:
-                current_power = threshold_workout_object.get_power_data()[-1]
-                power_usage += current_power
-                print("Current power: ", current_power)
-                power_usage_per_sec = threshold_workout_object.calculate_power_usage_per_second(start_time, power_usage)
-                print("Power usage per second: ", power_usage_per_sec)
-                if current_power > threshold_workout_object.threshold_power:
-                    print("Threshold power exceeded!")
-                    resistance_required = threshold_workout_object.calculate_resistance_required(current_power)
-                    print("Resistance required to reach threshold power: ", resistance_required)
+            if threshold_object.current_power is not None:
+                print('Current power:', threshold_object.current_power)
+                print('Threshold power: ', threshold_object.get_threshold_power())
             else:
-                print("No power data received")
-
+                print('No power data received')
+                
     except KeyboardInterrupt:
-        print("Test stopped")
-        print("Count of data points given: " + str(len(threshold_workout_object.get_power_data())))
-        pass
+        print('Workout stopped')
+        print('Count of data points given: ' + str(len(threshold_object.power_data)))
+        mqtt_client.get_client().loop_stop()
+        exit()
 
-
-
-def set_workout_duration(threshold_workout_object):
-    if len(sys.argv) > 1:
-        threshold_workout_object.set_duration(int(sys.argv[1]))
-        print("Duration set to " + str(sys.argv[1]) + " minutes")
+def user_input(threshold_object):
+    # User input for interval
+    interval = input('Enter amount of interval: ')
+    if interval == '':
+        print('No input given, defaulting to 3 interval')
+        threshold_object.set_interval(3)
     else:
-        threshold_workout_object.set_duration(20)
-        print("Duration not specified, defaulting to 20 minutes")
-
-def set_threshold_power(threshold_workout_object):
-    if len(sys.argv) > 2:
-        threshold_workout_object.set_threshold_power(int(sys.argv[2]))
-        print("Threshold power set to " + str(sys.argv[2]) + " watts")
+        print('Interval is set to', interval, 'intervals')
+        threshold_object.set_interval(int(interval))
+        
+    # User input for duration
+    duration = input('Enter workout duration for each interval (in minutes): ')
+    if duration == '':
+        print('No input given, defaulting to 5 minutes')
+        threshold_object.set_duration(5)
     else:
-        threshold_workout_object.set_threshold_power(200)
-        print("Threshold power not specified, defaulting to 200 watts")
+        print('Duration is set to', duration, 'minutes')
+        threshold_object.set_duration(int(duration))
 
+    # User input for rest
+    rest = input('Enter rest duration between each intervals (in seconds): ')
+    if rest == '':
+        print('No input given, defaulting to 30 seconds')
+        threshold_object.set_rest(30)    
+    else:
+        print('Rest is set to', rest, 'seconds')
+        threshold_object.set_rest(int(rest))
+        
+    # user input for threshold power
+    threshold_power = input('Enter threshold power (in Watts): ')
+    if threshold_power == '':
+        print('No input giver, defaulting to 200 Watts')
+        threshold_object.set_threshold_power(200)
+    else:
+        print('Threshold power is set to', threshold_power, 'Watts')
+        threshold_object.set_threshold_power(int(threshold_power))
+
+def start_workout(threshold_object):
+    
+    # Start Threshold workout, looping for amount of interval
+    for i in range(threshold_object.get_interval()):
+        print('Starting threshold workout')
+        print('Interval number: ', i+1)
+        perform_threshold_workout(threshold_object)
+        print('Interval number', i+1, 'complete')
+        # Rest between each interval, in the last interval no rest
+        if i+1 != threshold_object.get_interval():
+            print('Resting for', threshold_object.get_rest(), 'seconds')
+            time.sleep(threshold_object.get_rest())
+            
+    print('Congratulations on finishing your workout!!!')
+    # possible improvement, create calculation, print result
+    
 def main():
     try:
         env_path = '/home/pi/.env'
         load_dotenv(env_path)
-        threshold_workout_object = ThresholdWorkout()  # Updated class name
-        threshold_workout_object.__init__()  # Removed redundant initialization
+        threshold_object = ThresholdWorkout()  # Create threshold object
+        threshold_object.__init__()
         global mqtt_client
         global deviceId
-        set_workout_duration(threshold_workout_object)
-        set_threshold_power(threshold_workout_object)
         
+        # Create user input
+        print('-----Welcome to threshold workout-----')
+        user_input(threshold_object)
+        
+        # Initialize MQTT client and subcribe to power topic
         mqtt_client = MQTTClient(os.getenv('MQTT_HOSTNAME'), os.getenv('MQTT_USERNAME'), os.getenv('MQTT_PASSWORD'))
         deviceId = os.getenv('DEVICE_ID')
-        topic = f'bike/{deviceId}/power'
+        topic1 = f'bike/{deviceId}/power'
+        # topic2 = f'bike/{deviceId}/speed'
         print(deviceId)
-        print(topic)
+        print(topic1)
+        # print(topic2)
         mqtt_client.setup_mqtt_client()
-        mqtt_client.subscribe(topic)
-        mqtt_client.get_client().on_message = threshold_workout_object.read_remote_data
+        mqtt_client.subscribe(topic1)
+        # mqtt_client.subscribe(topic2)
+        mqtt_client.get_client().on_message = threshold_object.read_message
         mqtt_client.get_client().loop_start()
+        time.sleep(1)
         
-        print("Starting the threshold test...")
-        perform_threshold_test(threshold_workout_object)  # Updated variable name
-        
+        # Start workout program
+        start_workout(threshold_object)
+                
     except KeyboardInterrupt:
         pass
+    
     mqtt_client.get_client().loop_stop()
-
-if __name__ == "__main__":
+    
+if __name__ == '__main__':
     main()
