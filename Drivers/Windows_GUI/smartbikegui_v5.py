d6e1ff25fe5a5aa92ef9252fc0b8b1a838dec467
JarrodYong <41532177+JarrodYong@users.noreply.github.com>
2023-08-29 11:58:57 +1000

    Changes to Smartbike GUI
    MQTT implemented still have yet to test it though will be doing it on Thursday the 31st of August

--- Drivers/Windows_GUI/smartbikegui_v5.py
+++ Drivers/Windows_GUI/smartbikegui_v5.py
@@ -2,34 +2,21 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 import time
-import paho.mqtt.client as mqtt
+from mqtt_client import MQTTClient
 
 # Global variables for GUI
 resistance_var = None
 incline_var = None
 speed_var = None
 distance_var = None
+power_var = None  # Declare power_var as a global variable
+countdown_var = None  # Declare countdown_var as a global variable
 
 # Global variables for simulation
 training_active = False
+countdown_active = False
 start_time = 0
 distance_traveled = 0
-
-# MQTT client setup
-mqtt_client = mqtt.Client()
-
-def on_message(client, userdata, message):
-    # Handle incoming MQTT message here
-    # Update resistance, incline, or other parameters based on the received message
-    pass
-
-mqtt_client.on_message = on_message
-
-def setup_mqtt():
-    mqtt_client.connect("mqtt_broker_address", 1883)
-    mqtt_client.subscribe("topic/resistance")
-    mqtt_client.subscribe("topic/incline")
-    mqtt_client.loop_start()
 
 def update_values():
     global start_time, distance_traveled
@@ -49,9 +36,13 @@ def update_values():
         # Calculate distance traveled based on speed and elapsed time
         distance_traveled = speed * elapsed_time / 3600  # Convert seconds to hours
 
+        # Calculate power using speed and resistance
+        power = calculate_power(speed, resistance)
+
         # Update GUI with calculated values
         speed_var.set(f"{speed} km/h")
         distance_var.set(f"{distance_traveled:.2f} km")
+        power_var.set(f"{power:.2f} Watts")  # Update power value
 
         # Publish current resistance and incline values to MQTT topics
         mqtt_client.publish("topic/resistance", resistance)
@@ -60,18 +51,73 @@ def update_values():
     # Schedule the next update after a delay (in milliseconds)
     root.after(1000, update_values)
 
-# Rest of the code remains unchanged
-# ...
+def on_resistance_change(event):
+    # Update resistance value from user input
+    try:
+        resistance = int(resistance_entry.get())
+        if 0 <= resistance <= 10:
+            resistance_var.set(resistance)
+        else:
+            raise ValueError
+    except ValueError:
+        resistance_entry.delete(0, tk.END)
+        resistance_var.set("Invalid Input")
+
+def on_incline_change(event):
+    # Update incline value from user input
+    try:
+        incline = int(incline_entry.get())
+        if 0 <= incline <= 10:
+            incline_var.set(incline)
+        else:
+            raise ValueError
+    except ValueError:
+        incline_entry.delete(0, tk.END)
+        incline_var.set("Invalid Input")
+
+def start_training():
+    global training_active, countdown_active, start_time, distance_traveled
+    if not training_active:
+        training_active = True
+        countdown_active = True
+        start_time = time.time()  # Record the start time of pedaling
+        distance_traveled = 0  # Reset distance traveled
+        countdown_timer()
+        update_values()
+
+def stop_training():
+    global training_active, countdown_active, start_time, distance_traveled
+    if training_active:
+        training_active = False
+        countdown_active = False
+        start_time = 0  # Reset the start time
+        distance_traveled = 0  # Reset distance traveled
+        speed_var.set("0 km/h")  # Reset speed display
+        distance_var.set("0.00 km")  # Reset distance display
+        countdown_var.set("5:00")  # Reset the countdown display
+
+def countdown_timer():
+    global countdown_active
+    if countdown_active:
+        remaining_time = 300 - int(time.time() - start_time)
+        if remaining_time <= 0:
+            stop_training()
+            return
+        countdown_var.set(f"{remaining_time // 60}:{remaining_time % 60:02}")
+        root.after(1000, countdown_timer)
+    else:
+        countdown_var.set("5:00")  # Reset the countdown display
+
 
 def main():
-    global root, resistance_var, incline_var, speed_var, distance_var, resistance_entry, incline_entry
+    global root, resistance_var, incline_var, speed_var, distance_var, resistance_entry, incline_entry, power_var, countdown_var
 
     root = tk.Tk()
     root.title("Smart Indoor Bike Dashboard")
 
       # Background Image
     bg_image = Image.open("cycling_path.jpg")
-    bg_image = bg_image.resize((800, 600), Image.ANTIALIAS)
+    #bg_image = bg_image.resize((800, 600), Image.ANTIALIAS)
     bg_photo = ImageTk.PhotoImage(bg_image)
     bg_label = tk.Label(root, image=bg_photo)
     bg_label.place(x=0, y=0)
@@ -108,6 +154,13 @@ def main():
     distance_value_label = tk.Label(root, textvariable=distance_var, font=("Helvetica", 16))
     distance_value_label.place(x=170, y=160)
 
+    # Countdown Label
+    countdown_label = tk.Label(root, text="Countdown:", font=("Helvetica", 16))
+    countdown_label.place(x=30, y=240)
+    countdown_var = tk.StringVar()
+    countdown_value_label = tk.Label(root, textvariable=countdown_var, font=("Helvetica", 16))
+    countdown_value_label.place(x=170, y=240)
+
     # Start Button
     start_button = ttk.Button(root, text="START", command=start_training)
     start_button.place(x=50, y=210)
@@ -121,6 +174,21 @@ def main():
     root.after(1000, update_values)  # Start the update loop
 
     root.mainloop()
+
+   # Initialize MQTT client and subscribe to power topic
+        mqtt_client = MQTTClient(os.getenv('MQTT_HOSTNAME'), os.getenv('MQTT_USERNAME'), os.getenv('MQTT_PASSWORD'))
+        deviceId = os.getenv('DEVICE_ID')
+        topic = f'bike/{deviceId}/power'
+        print(deviceId)
+        print(topic)
+        mqtt_client.setup_mqtt_client()
+        mqtt_client.subscribe(topic)
+        mqtt_client.get_client().on_message = ftp_object.read_remote_data
+        mqtt_client.get_client().loop_start()
+
+     except KeyboardInterrupt:
+        pass
+    mqtt_client.get_client().loop_stop()
 
 if __name__ == "__main__":
     main()
