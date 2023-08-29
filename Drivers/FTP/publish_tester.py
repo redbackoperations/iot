914cfc8f61cd5c74ec716120ffd3ce21170b3d14
JarrodYong <41532177+JarrodYong@users.noreply.github.com>
2023-08-29 12:02:28 +1000

    Update publish_tester.py

--- Drivers/FTP/publish_tester.py
+++ Drivers/FTP/publish_tester.py
@@ -15,7 +15,7 @@ load_dotenv(vars)
 mqtt_client = MQTTClient('f5b2a345ee944354b5bf1263284d879e.s1.eu.hivemq.cloud', 'redbackiotclient', 'IoTClient@123')
 mqtt_client.setup_mqtt_client()
 deviceId = os.getenv('DEVICE_ID')
-mqtt_client.subscribe(f"bike/000001/heartrate")
+mqtt_client.subscribe(f"bike/000001/power")
 mqtt_client.get_client().on_message = message
 mqtt_client.get_client().loop_start()
 
