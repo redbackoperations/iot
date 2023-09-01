09f0be83c0a62f3397b835a678dfc1c5de90fe7e
William Djojodiredjo <williamdjojodiredjo@gmail.com>
2023-09-01 18:50:06 +1000

    Update threshold workout program
    Now able to calculate distance travelled and calories burnt

--- Drivers/Threshold_workout/Threshold_workout.py
+++ Drivers/Threshold_workout/Threshold_workout.py
@@ -20,8 +20,10 @@ def perform_threshold_workout(threshold_object):
         while time.time() - start_time < (threshold_object.get_duration()*60):
             time.sleep(1)
             if threshold_object.current_power is not None:
+                print('Current Speed:', threshold_object.current_speed)
                 print('Current power:', threshold_object.current_power)
                 print('Threshold power: ', threshold_object.get_threshold_power())
+                print('-------------------------------')
             else:
                 print('No power data received')
                 
@@ -33,11 +35,11 @@ def perform_threshold_workout(threshold_object):
 
 def user_input(threshold_object):
     """
-    Example input: python Threshold_workout a b c d, where 
+    Example input: python Threshold_workout.py a b c d, where 
     a -> interval
-    b -> duration
-    c -> rest
-    d -> threshold power
+    b -> duration (in minutes)
+    c -> rest (in seconds)
+    d -> threshold power (in Watts)
     """
     # Interval
     threshold_object.set_interval(int(sys.argv[1]))
@@ -69,7 +71,12 @@ def start_workout(threshold_object):
             time.sleep(threshold_object.get_rest())
             
     print('Congratulations on finishing your workout!!!')
-    # possible improvement, create calculation, print result
+    # possible improvement - create calculation, print result
+    threshold_object.calculate_distance()
+    threshold_object.calculate_calories()
+    print('-----Your Result-----')
+    print('Distance travelled:', threshold_object.get_distance(), 'km')
+    print('Calories burnt:', threshold_object.get_calories(), 'kcal')
     
 def main():
     try:
@@ -88,13 +95,13 @@ def main():
         mqtt_client = MQTTClient(os.getenv('MQTT_HOSTNAME'), os.getenv('MQTT_USERNAME'), os.getenv('MQTT_PASSWORD'))
         deviceId = os.getenv('DEVICE_ID')
         topic1 = f'bike/{deviceId}/power'
-        # topic2 = f'bike/{deviceId}/speed'
+        topic2 = f'bike/{deviceId}/speed'
         print(deviceId)
         print(topic1)
-        # print(topic2)
+        print(topic2)
         mqtt_client.setup_mqtt_client()
         mqtt_client.subscribe(topic1)
-        # mqtt_client.subscribe(topic2)
+        mqtt_client.subscribe(topic2)
         mqtt_client.get_client().on_message = threshold_object.read_message
         mqtt_client.get_client().loop_start()
         time.sleep(1)
